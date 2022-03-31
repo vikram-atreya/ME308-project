1885f772d222f05615ea332a750f3967ef3ad149
Vikram Atreya <55907158+vikram-atreya@users.noreply.github.com>
2022-03-31 14:11:04 +0530

    Added till eq 10

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class Ship:
 
 bays = 1
 stacks = 1000
+R = stacks-1
 tiers = 1000
 
 stow = [[[0 for k in range(bays)] for j in range(stacks)] for i in range(tiers)]
@@ -54,7 +55,7 @@ for i in range(n_port):
             temp_list.append(0)
     T.append(temp_list)
 
-print(T)
+#print(T)
 
 prob = LpProblem("Stowage",LpMinimize)
 
@@ -69,8 +70,8 @@ for i in range(n_port):
                 upperBound = 1
                 if i == n_port-1:
                     upperBound = 0
-                x[i,j] = pulp.LpVariable('x' + str(i) + '_' + str(j) + '_' + str(s) + '_' + str(t), lowerBound, upperBound, pulp.LpBinary)
-                y[i,j] = pulp.LpVariable('y' + str(i) + '_' + str(j) + '_' + str(s) + '_' + str(t), lowerBound, upperBound, pulp.LpBinary)
+                x[i,j] = pulp.LpVariable('x' + str(i) + '_' + str(j) + '_' + str(s) + '_' + str(t), lowerBound, upperBound)
+                y[i,j] = pulp.LpVariable('y' + str(i) + '_' + str(j) + '_' + str(s) + '_' + str(t), lowerBound, upperBound)
 
 prob += pulp.LpSum([ y[i,j,s,t] for i in range(2,n_port-1) for j in range(i,n_cont) for s in range(stacks) for t in range(tiers)])
 
@@ -86,9 +87,23 @@ for i in range(n_port-1):
             prob += x[i-1,i,s-1,t] + pulp.LpSum([ y[i,j,s-1,t] for j in range(i+1,n_port)]) <=  x[i-1,i,s,t] + pulp.LpSum([ y[i,j,s,t] for j in range(i+1,n_port)])
 
 for j in range(n_port-1):
-            prob += pulp.LpSum( [x[1,j,s,t] for s in range(stacks) for t in range(tiers)]) == T[1][j]
+    prob += pulp.LpSum( [x[1,j,s,t] for s in range(stacks) for t in range(tiers)]) == T[1][j]
 
+for i in range(n_port-1):
+    for j in range(i,n_cont):
+        prob += pulp.LpSum( [x[i,j,s,t]-x[i-1,j,s,t] for s in range(stacks) for t in range(tiers)]) == T[i][j]
 
+for i in range(n_port-1):
+    for s in range(1,stacks):
+        for t in range(tiers):
+            prob += pulp.LpSum( [x[i,j,s,t] for j in range(i+1,n_port) for t in range(tiers)]) <=1
+
+for i in range(2,n_port):
+    prob += pulp.LpSum( [x[i-1,i,R,t] for t in range(tiers)]) + pulp.LpSum( [y[i,j,R,t] for j in range(i+1,n_port) for t in range(tiers)]) <= [T[p][i] for p in range(1,i)]/R
+
+for i in range(2,n_port):
+    for s in range(1,stacks):
+        prob += pulp.LpSum( [x[i-1,i,r,t] for r in range(s,R+1) for t in range(tiers)]) + pulp.LpSum( [y[i,j,r,t] for r in range(s,R+1) for j in range(i+1,n_port) for t in range(tiers)]) >= (R-s+1)*[T[p][i] for p in range(1,i)]/R  ##min term ayega
 
 prob.solve()
 
